2f89d46b5bc0ab9b0b03f7134f68f9fe103dc87a
mehdimessaoud <mohamedmehdi.messaoud@esprit.tn>
2020-02-13 23:06:09 +0100

    adding buttons in the homepage

--- lib/Home.dart
+++ lib/Home.dart
@@ -14,7 +14,59 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         elevation: 0,
       ),
+      body:
 
+      GridView.count(primary: false,
+
+      padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        crossAxisCount: 2,
+
+        children: <Widget>[
+
+          new InkWell(
+          onTap: (){
+    print("Container clicked");
+    },
+        child: new Card(  
+          clipBehavior: Clip.antiAlias,
+           shape: RoundedRectangleBorder(
+             borderRadius: BorderRadius.circular(8.0)
+           ),
+
+            color: Colors.teal[300],
+           child: new Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+
+                new Text("Gratitude beads"),
+              ]
+          ),
+        )
+    ),
+
+          InkWell(
+            onTap: (){},
+            child: new Card(
+              color: Colors.teal[300],
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0)
+              ),
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  new Text("Gratitude Journal")
+                ],
+              ),
+            ),
+          ),
+
+
+        ],
+      )
     );
   }
 }
